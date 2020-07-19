a33d7204f57e0fe6036ed687ed30f1aad8e22fed
aliciaajy <65158683+aliciaajy@users.noreply.github.com>
2020-07-19 19:23:30 +0800

    created settings page to facilitate signing out etc

---
+++ lib/pages/settings.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:login/pages/login_page.dart';
+
+class Settings extends StatelessWidget {
+  final _firebaseAuth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: new AppBar(
+          title: new Text('Settings'),
+          backgroundColor: Colors.purple,
+          centerTitle: true,
+        ),
+        body: new ListView(children: <Widget>[
+          new ListTile(
+            title: new Text('Profile'),
+            isThreeLine: true,
+            subtitle: new Text('Edit profile'),
+            trailing: new Icon(Icons.account_box),
+          ),
+          new ListTile(
+            title: new Text('Password'),
+            isThreeLine: true,
+            subtitle: new Text('Change password'),
+            trailing: new Icon(Icons.lock_outline),
+          ),
+          new ListTile(
+            title: new Text('Background Design'),
+            isThreeLine: true,
+            subtitle: new Text('Edit background image'),
+            trailing: new Icon(
+              Icons.format_color_fill,
+            ),
+            onTap: () {},
+          ),
+          new ListTile(
+              title: new Text('Progress'),
+              isThreeLine: true,
+              subtitle: new Text(
+                  'Check your percentage of work done and the progress of your lists'),
+              trailing: new Icon(Icons.hourglass_empty)),
+          new ListTile(
+              title: new Text('Sign Out'),
+              subtitle: new Text('Log out of your account'),
+              isThreeLine: true,
+              trailing: new Icon(Icons.directions),
+              onTap: () async {
+                await _firebaseAuth.signOut();
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => LoginPage(),
+                    ));
+              }),
+        ]));
+  }
+}
--- lib/pages/type_of_list.dart
+++ lib/pages/type_of_list.dart
@@ -4,36 +4,37 @@ import 'package:login/taskpages/task_home_page.dart';
 import 'package:login/shoppingpages/shoppinglist_home_page.dart';
 import 'package:login/cookingpages/cookinglist_home_page.dart';
 import 'package:login/travelpages/travelList_homepage.dart';
+import 'package:login/pages/settings.dart';
 
 class Option extends StatelessWidget {
-  @override 
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Type of List",
       home: Scaffold(
         appBar: PreferredSize(
-          preferredSize:Size.fromHeight(80),
-          child: AppBar(title:Text("Choose Your List",
-          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-          backgroundColor: Colors.purple,
-          actions: <Widget> [
-          Icon(Icons.settings, size:25.0)
-        ],
-          elevation: 10.0,
-          leading:Icon(Icons.menu, size:25),
-          )
-
+          preferredSize: Size.fromHeight(80),
+          child: AppBar(
+              title: Text("Choose Your List",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+              backgroundColor: Colors.purple,
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.settings),
+                  onPressed: () => Navigator.of(context).push(
+                      new MaterialPageRoute(
+                          builder: (BuildContext context) => new Settings())),
+                )
+              ]),
+        ),
+        body: ListOption(),
       ),
-        body: 
-
-         ListOption(),
-         ),
     );
   }
 }
 
 class ListOption extends StatelessWidget {
-  @override 
+  @override
   Widget build(BuildContext context) {
     return _myListOption(context);
   }
@@ -41,95 +42,144 @@ class ListOption extends StatelessWidget {
 
 Widget _myListOption(BuildContext context) {
   return Column(
-     mainAxisAlignment: MainAxisAlignment.center,
-     crossAxisAlignment: CrossAxisAlignment.center,
-    children:  <Widget> [
+    mainAxisAlignment: MainAxisAlignment.center,
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: <Widget>[
+      Container(
+          height: 140,
+          child: Card(
+            elevation: 50,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+            child: ListTile(
+              leading: Icon(Icons.list, size: 80, color: Colors.purpleAccent),
+              title: Text(
+                "To Do List",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text("List your tasks and plan your day with us",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontStyle: FontStyle.italic)),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MyTaskHomePage(),
+                    ));
+              },
+            ),
+          )),
       Container(
         height: 140,
         child: Card(
-        elevation: 50,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-        child: ListTile(
-          leading: Icon(Icons.list, size:80, color: Colors.purpleAccent),
-          title: Text("To Do List", textAlign: TextAlign.left,style: TextStyle(color: Colors.black,fontSize:25, fontWeight: FontWeight.bold),),
-          subtitle: Text("List your tasks and plan your day with us", textAlign: TextAlign.left, style: TextStyle(color: Colors.black,fontSize:20, fontStyle: FontStyle.italic)),
-          trailing: Icon(Icons.keyboard_arrow_right),
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => MyTaskHomePage(),)
-            );
-          },
+          elevation: 50,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+          child: ListTile(
+            leading: Icon(Icons.local_grocery_store,
+                size: 80, color: Colors.purpleAccent),
+            title: Text(
+              "Shopping List",
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold),
+            ),
+            subtitle: Text("List your items and shop together with us",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontStyle: FontStyle.italic)),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ShoppingListHomePage(),
+                  ));
+            },
+          ),
         ),
-        )
-        
       ),
       Container(
-
-        height:140,
+        height: 140,
         child: Card(
-        elevation: 50,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-        child: ListTile(
-          leading: Icon(Icons.local_grocery_store, size:80,color: Colors.purpleAccent),
-          title: Text("Shopping List",textAlign: TextAlign.left,style: TextStyle(color: Colors.black,fontSize:25, fontWeight: FontWeight.bold),),
-          subtitle: Text("List your items and shop together with us", textAlign: TextAlign.left, style: TextStyle(color: Colors.black,fontSize:20, fontStyle: FontStyle.italic)),
-          trailing: Icon(Icons.keyboard_arrow_right),
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => ShoppingListHomePage(),)
-            );
-          },
+          elevation: 50,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+          child: ListTile(
+              leading: Icon(
+                Icons.fastfood,
+                size: 80,
+                color: Colors.purpleAccent,
+              ),
+              title: Text(
+                "Cooking List",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text("List your recipes and cook with us",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontStyle: FontStyle.italic)),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => CookingListHomePage(),
+                    ));
+              }),
         ),
-      ),
       ),
       Container(
-
-        height:140,
-        child:Card(
-        elevation: 50,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-        child: ListTile(
-          leading: Icon(Icons.fastfood, size:80, color: Colors.purpleAccent,),
-          title: Text("Cooking List",textAlign: TextAlign.left,style: TextStyle(color: Colors.black,fontSize:25, fontWeight: FontWeight.bold),),
-          subtitle: Text("List your recipes and cook with us", textAlign: TextAlign.left, style: TextStyle(color: Colors.black,fontSize:20, fontStyle: FontStyle.italic)),
-          trailing: Icon(Icons.keyboard_arrow_right),
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => CookingListHomePage(),)
-            );
-          }
+        height: 140,
+        child: Card(
+          elevation: 50,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+          child: ListTile(
+              leading: Icon(Icons.airplanemode_active,
+                  size: 80, color: Colors.purpleAccent),
+              title: Text(
+                "Travel List",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text("List your itineraries and travel with us",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontStyle: FontStyle.italic)),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => TravelListHomePage(),
+                    ));
+              }),
         ),
-      ),
-      ),
-      Container(height: 140,
-  
-      child: Card(
-        elevation: 50,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-        child: ListTile(
-          leading: Icon(Icons.airplanemode_active,size:80,color: Colors.purpleAccent),
-          title: Text("Travel List",textAlign: TextAlign.left,style: TextStyle(color: Colors.black,fontSize:25, fontWeight: FontWeight.bold),), 
-          subtitle: Text("List your itineraries and travel with us", textAlign: TextAlign.left, style: TextStyle(color: Colors.black,fontSize:20, fontStyle: FontStyle.italic)),
-          trailing: Icon(Icons.keyboard_arrow_right),
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => TravelListHomePage(),)
-            );
-          }
-        ),
-      ),
       ),
     ],
   );
 }
-
-
-
